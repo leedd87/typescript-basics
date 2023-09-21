db21daa221bb0fbbfa454407f418c78b8179bf18
leedd87 <leedav87@gmail.com>
2023-09-21 20:14:02 -0300

    counter in tsx

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,3 +1,4 @@
+import { Counter } from "./components/Counter";
 
 
 function App() {
@@ -6,6 +7,9 @@ function App() {
     return (
         <>
             <h1>React + Typescript</h1>
+            <hr />
+
+            <Counter />
         </>
     );
 }
